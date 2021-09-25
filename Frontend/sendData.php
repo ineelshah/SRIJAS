0282c28ecad58a64c278396a193032d3ffb1f678
Harshil Shah <hsharshil@gmail.com>
2021-09-25 15:40:02 -0400

    added backend connection test

---
+++ Frontend/sendData.php
@@ -0,0 +1,41 @@
+<?php
+$inputName = $_POST["inputName"];
+$inputEmail = $_POST["inputEmail"];
+// $jobType = $_POST["inputJobTypeId"];
+$inputJobTypeId = 1;
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "srijas";
+$conn = new mysqli($servername, $username, $password, $db);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+// $sql = "SELECT * FROM user_master";
+// $result = $conn->query($sql);
+// $last_id = result->num_rows
+// if ($result->num_rows > 0) {
+//
+//   while($row = $result->fetch_assoc()) {
+//     echo $row["Name"];
+//   }
+// } else {
+//   echo "0 results";
+// }
+$names = explode(" ", $inputName);
+$firstName = $names[0];
+$lastName = $names[1];
+$active = 1;
+$created_by = -1;
+// $updated_by = "Admin";
+
+$stmt = $conn->prepare("INSERT INTO user_master (user_fname, user_lname, user_email, user_preferred_job_id, is_active, created_by)
+  VALUES (?, ?, ?, ?, ?, ?)");
+
+$stmt->bind_param("sssiii", $firstName, $lastName, $inputEmail, $inputJobTypeId, $active, $created_by);
+$stmt->execute();
+$stmt->close();
+$conn->close();
+?>

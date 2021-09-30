1a1fcd11383994d48940726935a12467ea8b0063
Neel Shah <neelshah.2798@gmail.com>
2021-09-29 22:15:23 -0400

    Added basic tests.

--- tests/keyword_extraction_modules_test.py
+++ tests/keyword_extraction_modules_test.py
@@ -1,7 +1,15 @@
 import pytest
+import sys
+sys.path.append("../Code/Scrapper")
+from keyword_extraction_modules import get_user_id_to_list_of_job_ids
 
-def testable_function(x):
-  return 2 * x
 
-def test_testable_function():
-  assert testable_function(2) == 4
+resume_skills_dict = {"res1": [1, 2, 3, 4]}
+links_description_dict = {"link1": "This description has java and python class."}
+total_skills = {1: "Java", 2: "C"}
+threshold = 50
+db_connection = ""
+
+
+def test_main_function():
+  assert get_user_id_to_list_of_job_ids(resume_skills_dict, links_description_dict, db_connection, total_skills, threshold) == {'res1': ['link1']}

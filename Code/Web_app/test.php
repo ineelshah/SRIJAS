2baadefd9050ac92b44be8107271d6bae4339bf4
Het Patel <phet0199@gmail.com>
2021-11-03 17:32:11 -0400

    added minor fixes.

--- Code/Web_app/test.php
+++ Code/Web_app/test.php
@@ -1,30 +1,37 @@
 <?php
-ob_start();
-require 'sendData.php';
-$results = executer("Hardik", "hvudeshi@ncsu.edu", "Hardik", 1, "Hardik_Udeshi_Resume.pdf");
-ob_end_clean();
-if($results["resume"]==1){
-  $message2 = "Resume test cases passed successfully.";
-}else{
-  $message2 = "Resume test cases failed.";
-}
-if($results["user"]==1){
-  $message1 = "User test cases passed successfully.";
-}else{
-  $message1 = "User test cases failed.";
-}
-if($results["params"]==1){
-  $message3 = "Dynamic infrastructure Parameter File has been created successfully.";
-}else{
-  $message3 = "Dynamic infrastructure Parameter File creation failed.";
-}
-if($results["dbConnection"]==1){
-  $message4 = "Overall infrastructure and database connection is checked successfully";
-}else{
-  $message4 = "Issue in infrastructure setting and database connection";
-}
+  ob_start();
+  require 'sendData.php';
+  $results = executer("Hardik", "hvudeshi@ncsu.edu", "Hardik", 1, "Hardik_Udeshi_Resume.pdf");
+  ob_end_clean();
+  if($results["resume"]==1){
+    $message2 = "Resume test cases passed successfully.";
+  }
+  else{
+    $message2 = "Resume test cases failed.";
+  }
 
-$message = $message1.", \n ".$message2.", \n ".$message3.", \n ".$message4.".";
+  if($results["user"]==1){
+    $message1 = "User test cases passed successfully.";
+  }
+  else{
+    $message1 = "User test cases failed.";
+  }
+  
+  if($results["params"]==1){
+    $message3 = "Dynamic infrastructure Parameter File has been created successfully.";
+  }
+  else{
+    $message3 = "Dynamic infrastructure Parameter File creation failed.";
+  }
+  
+  if($results["dbConnection"]==1){
+    $message4 = "Overall infrastructure and database connection is checked successfully";
+  }
+  else{
+    $message4 = "Issue in infrastructure setting and database connection";
+  }
+
+  $message = $message1.", \n ".$message2.", \n ".$message3.", \n ".$message4.".";
 // $message = wordwrap($message, 70, "\r\n");
 //
 // $headers = array(
@@ -39,9 +46,9 @@ $message = $message1.", \n ".$message2.", \n ".$message3.", \n ".$message4.".";
 //   echo "Mail could not be sent.";
 // }
 
-$message_param = 'python3 mail_test.py "'.$message.'"';
-$command = escapeshellcmd($message_param);
-$output = shell_exec($command);
-echo $output;
+  $message_param = 'python3 mail_test.py "'.$message.'"';
+  $command = escapeshellcmd($message_param);
+  $output = shell_exec($command);
+  echo $output;
 
 ?>

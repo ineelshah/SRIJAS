73286013487492baebf8fe4036dc39e08cd944d5
sal0ni <37000199+sal0ni@users.noreply.github.com>
2021-11-04 16:12:52 -0400

    Update dbConnect.py

--- Code/Scrapper/dbConnect.py
+++ Code/Scrapper/dbConnect.py
@@ -1,10 +1,11 @@
 import pymysql
 
+#Connect to the database 'srijas'
 host = 'srijas.cgdwdytucgi0.us-east-1.rds.amazonaws.com'
 user = 'root'
 password = ''
 database = 'srijas'
-
+#Run the connection command and execute the verion of the database
 connection = pymysql.connect(host = host, user = user, password = password, database = database)
 with connection:
     cur = connection.cursor()
